d73e9add02ad57805bf9102007666e716e749353
Joshua Treudler <joshua@treudler.net>
2023-12-28 16:58:47 +0100

    Add NeueSeite component

---
+++ www/pages/NeueSeite.js
@@ -0,0 +1,12 @@
+// Datei: src/pages/NeueSeite.js
+import React from 'react';
+
+const NeueSeite = () => {
+  return (
+    <div>
+      <h1>Willkommen auf der neuen Seite!</h1>
+    </div>
+  );
+};
+
+export default NeueSeite;
