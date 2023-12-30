7f9afb8ba7e3ab7c444cb89eaafa81dabfddd32b
Joshua Treudler <joshua@treudler.net>
2023-12-30 21:15:55 +0100

    Remove deprecated options from mongoose.connect() call

--- www/private/core/database/database.js
+++ www/private/core/database/database.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/casfra', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
   })
   .then(() => {
     console.log('Connected to MongoDB');
