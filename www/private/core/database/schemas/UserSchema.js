8e78919d39f77a1b7efbc3fbcd868608486d3674
Joshua Treudler <joshua@treudler.net>
2023-12-30 19:22:56 +0100

    fix: missing module exports for User

--- www/private/core/database/schemas/UserSchema.js
+++ www/private/core/database/schemas/UserSchema.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const generateRandomPriority = require('../../utils/generateRandomPriority');
 
 
-
 // Define User schema
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: {
       type: String,
       required: true,
@@ -58,4 +57,6 @@ const userSchema = new mongoose.Schema({
     tenancy: String,
   });
   
-  const User = mongoose.model('User', userSchema);
+  const User = mongoose.model('User', UserSchema);
+
+  module.exports = User;
