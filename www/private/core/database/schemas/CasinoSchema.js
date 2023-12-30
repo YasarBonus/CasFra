4e9b2dcb95570ac518c16d00514d6f706928dc99
Joshua Treudler <joshua@treudler.net>
2023-12-30 21:30:04 +0100

    Refactor CasinoSchema.js to use ES6 syntax

--- www/private/core/database/schemas/CasinoSchema.js
+++ www/private/core/database/schemas/CasinoSchema.js
@@ -3,64 +3,64 @@ const generateRandomPriority = require('../../utils/generateRandomPriority');
 
 // Define Casino schema
 const casinoSchema = new mongoose.Schema({
-    name: String,
-    categories: [String],
-    description: String,
-    priority: {
-      type: Number,
-      default: generateRandomPriority()
-    },
-    addedDate: {
-      type: Date,
-      default: Date.now,
-      required: true
-    },
-    addedBy: String,
-    modifiedDate: Date,
-    modifiedBy: String,
-    active: {
-      type: Boolean,
-      default: false
-    },
-    newCasino: {
-      type: Boolean,
-      default: false
-    },
-    label: String,
-    labelLarge: String,
-    individualBonuses: [String],
-    displayBonus: String,
-    maxBet: Number,
-    maxCashout: Number,
-    wager: Number,
-    wagerType: [String],
-    noDeposit: {
-      type: Boolean,
-      default: false
-    },
-    prohibitedGamesProtection: {
-      type: Boolean,
-      default: true
-    },
-    vpn: {
-      type: Boolean,
-      default: false
-    },
-    features: [String],
-    individualFeatures: [String],
-    providers: [String],
-    paymentMethods: [String],
-    reviewTitle: String,
-    review: String,
-    image: String,
-    imageUrl: String,
-    affiliateUrl: String,
-    affiliateShortlink: String,
-    licenses: [String],
-    tags: [String],
-    tenancies: [String],
-  });
-  
-  const Casino = mongoose.model('Casino', casinoSchema);
+  name: String,
+  categories: [String],
+  description: String,
+  priority: {
+    type: Number,
+    default: generateRandomPriority()
+  },
+  addedDate: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  addedBy: String,
+  modifiedDate: Date,
+  modifiedBy: String,
+  active: {
+    type: Boolean,
+    default: false
+  },
+  newCasino: {
+    type: Boolean,
+    default: false
+  },
+  label: String,
+  labelLarge: String,
+  individualBonuses: [String],
+  displayBonus: String,
+  maxBet: Number,
+  maxCashout: Number,
+  wager: Number,
+  wagerType: [String],
+  noDeposit: {
+    type: Boolean,
+    default: false
+  },
+  prohibitedGamesProtection: {
+    type: Boolean,
+    default: true
+  },
+  vpn: {
+    type: Boolean,
+    default: false
+  },
+  features: [String],
+  individualFeatures: [String],
+  providers: [String],
+  paymentMethods: [String],
+  reviewTitle: String,
+  review: String,
+  image: String,
+  imageUrl: String,
+  affiliateUrl: String,
+  affiliateShortlink: String,
+  licenses: [String],
+  tags: [String],
+  tenancies: [String],
+});
+
+const Casino = mongoose.model('Casino', casinoSchema);
 
 module.exports = Casino;
