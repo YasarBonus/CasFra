347ec2a072479dab8ced2769b2012bbe8ae221ee
Joshua Treudler <joshua@treudler.net>
2023-12-30 16:30:02 +0100

    Refactor API endpoint for retrieving user tenancy details

--- www/private/server.js
+++ www/private/server.js
@@ -1881,46 +1881,48 @@ app.get('/api/user/tenancies', checkPermissions('authenticate'), (req, res) => {
 
 // Get details of current tenancy of the current user
 app.get('/api/user/tenancy', checkPermissions('authenticate'), (req, res) => {
-  const {
-    userId
-  } = req.session.user;
+  const { userId } = req.session.user;
 
   User.findById(userId)
     .then((user) => {
       if (!user) {
-        res.status(404).json({
-          error: 'User not found'
-        });
+        res.status(404).json({ error: 'User not found' });
         return;
       }
 
-      const {
-        tenancy
-      } = user;
+      const { tenancy } = user;
 
       Tenancie.findById(tenancy)
         .then((result) => {
           if (!result) {
-            res.status(404).json({
-              error: 'Tenancy not found'
-            });
+            res.status(404).json({ error: 'Tenancy not found' });
             return;
           }
 
-          res.json(result);
+          const { tenancyId } = result;
+
+          TenanciesTypes.findById(tenancyId)
+            .then((tenancyType) => {
+              if (!tenancyType) {
+                res.status(404).json({ error: 'Tenancy type not found' });
+                return;
+              }
+
+              res.json({ tenancy: result, tenancyType });
+            })
+            .catch((error) => {
+              console.error('Error retrieving tenancy type:', error);
+              res.status(500).json({ error: 'Internal server error' });
+            });
         })
         .catch((error) => {
           console.error('Error retrieving tenancy:', error);
-          res.status(500).json({
-            error: 'Internal server error'
-          });
+          res.status(500).json({ error: 'Internal server error' });
         });
     })
     .catch((error) => {
       console.error('Error retrieving user details:', error);
-      res.status(500).json({
-        error: 'Internal server error'
-      });
+      res.status(500).json({ error: 'Internal server error' });
     });
 });
 
