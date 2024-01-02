4391dc03525bfeeda50a8d990acf9cb2d90e6dac
Joshua Treudler <joshua@treudler.net>
2024-01-02 20:37:23 +0100

    Add mongoose configuration file

---
+++ src/configs/mongooseConfig.js
@@ -0,0 +1,7 @@
+// dbConfig.js
+module.exports = {
+    url: 'mongodb://localhost:27017/casfra',
+    options: {
+      // Hier können Sie zusätzliche Optionen hinzufügen, wenn benötigt
+    },
+  };
