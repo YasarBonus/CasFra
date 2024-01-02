3b003c521f4fa2810e4bf65f8c079df8117e1960
Joshua Treudler <joshua@treudler.net>
2024-01-02 14:30:48 +0100

    cleanup, all yasar-related stuff has been moved away and we are no longer in a dirty subdirectory...

--- yasarbonus-kopie-kopie-kopie/private/core/utils/generateRandomPriority.js
+++
@@ -1,7 +0,0 @@
-// Function to generate a random priority
-const generateRandomPriority = () => {
-    const random = Math.floor(Math.random() * 100000000000000000000);
-    return random;
-}
-
-module.exports = generateRandomPriority;
