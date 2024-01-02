217ababeff5ebed706e54609a732bacf3d1ae4ea
Joshua Treudler <joshua@treudler.net>
2024-01-02 13:19:54 +0100

    Add error logging and timestamp to error response

--- www/private/core/modules/errorHandler.js
+++ www/private/core/modules/errorHandler.js
@@ -1,13 +1,32 @@
 const email = require('./emailService.js');
+const fs = require('fs');
 
 // Error handler middleware
 function errorHandler(err, req, res, next) {
     // Handle the error here
-    console.error('ERROR:', err);
+    logError(err, req, res, next);
     
+    const currentDateTime = new Date();
+
     // Send an appropriate response to the client
-    res.status(500).json({  code: res.statusCode, type: err.name, message: err.message });
-    email.sendEmail('joshua@treudler.net', 'System Error', 'An error occurred: ' + err.message + '\n\n' + err.stack);
+    res.status(500).json({  code: res.statusCode, type: err.name, message: err.message, date: currentDateTime, stack: err.stack });
+    // email.sendEmail('joshua@treudler.net', 'System Error', 'An error occurred: ' + err.message + '\n\n' + err.stack);
 }
+
+function logError(err, req, res, next) {
+    
+    // Handle the error here
+    console.error('ERROR:', err);
+
+
+    
+    // Log error to file
+    fs.appendFile('error.log', err.stack, function (error) {
+        if (error) {
+            console.error('Error logging error:', error);
+        }
+    });
+}
+
 
 module.exports = errorHandler;
