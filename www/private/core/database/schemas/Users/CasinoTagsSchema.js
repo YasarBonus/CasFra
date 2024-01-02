76e3ea6b9c297a57e0ae4fcbdde9603e2b2cfab4
Joshua Treudler <joshua@treudler.net>
2024-01-02 14:18:02 +0100

    move all User Schemas in their own folder

--- www/private/core/database/schemas/Users/CasinoTagsSchema.js
+++ www/private/core/database/schemas/Users/CasinoTagsSchema.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const generateRandomPriority = require('../../utils/generateRandomPriority');
+const generateRandomPriority = require('../../../utils/generateRandomPriority');
 
 
 // Define Casino tags schema
