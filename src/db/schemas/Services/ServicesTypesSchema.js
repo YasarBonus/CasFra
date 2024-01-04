dc294e82d8df247426da5424fec41115d6150d6d
Joshua Treudler <joshua@treudler.net>
2024-01-04 02:30:32 +0100

    Refactor ServicesTypesSchema and add initial data

--- src/db/schemas/Services/ServicesTypesSchema.js
+++ src/db/schemas/Services/ServicesTypesSchema.js
@@ -6,13 +6,39 @@ const ServicesTypesSchema = new mongoose.Schema({
         required: true,
         unique: true,
         trim: true,
-        active: {
-            type: Boolean,
-            default: true,
-        },
+    },
+    active: {
+        type: Boolean,
+        default: true,
     },
 });
 
 const ServicesTypes = mongoose.model('ServicesTypes', ServicesTypesSchema);
+
+const ServicesTypesData = [
+    {
+        name: 'VPS',
+        active: true,
+    },
+    {
+        name: 'Web Hosting',
+        active: true,
+    },
+    {
+        name: 'Domain',
+        active: true,
+    },
+    {
+        name: 'SSL',
+        active: false
+    },
+];
+
+ServicesTypesData.forEach(async (item) => {
+    const serviceType = await ServicesTypes.findOne({ name: item.name });
+    if (!serviceType) {
+        ServicesTypes.create(item);
+    }
+} );
 
 module.exports = ServicesTypes;
