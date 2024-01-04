56df688ed388089d9f6674142e427e00ac98a0c3
Joshua Treudler <joshua@treudler.net>
2024-01-04 18:18:25 +0100

    Update mongooseConfig.js to use environment variables for MongoDB URL

--- src/config/mongooseConfig.js
+++ src/config/mongooseConfig.js
@@ -1,7 +1,20 @@
-// dbConfig.js
+require('dotenv').config();
+
+let mongooseUrl = '';
+
+console.log('process.env.MODE_ENV', process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'development') {
+  mongooseUrl = process.env.MONGOOSE_DEV_URL;
+} else if (process.env.NODE_ENV === 'production') {
+  mongooseUrl = process.env.MONGOOSE_PROD_URL;
+}
+
 module.exports = {
-    url: 'mongodb://localhost:27017/casfra',
-    options: {
-      // Hier können Sie zusätzliche Optionen hinzufügen, wenn benötigt
-    },
-  };
+  url: mongooseUrl,
+  options: {
+    // Hier können Sie zusätzliche Optionen hinzufügen, wenn benötigt
+  },
+};
+
+// dbConfig.js
