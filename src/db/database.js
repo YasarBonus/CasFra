70418a50ddf118a7eb7b8586aec61f5a0ab2a9c1
Joshua Treudler <joshua@treudler.net>
2024-01-05 21:50:15 +0100

    Update import paths for schemas in database.js

--- src/db/database.js
+++ src/db/database.js
@@ -9,18 +9,18 @@ mongoose.connect(mongooseConfig.url, mongooseConfig.options)
     console.error('Error connecting to MongoDB:', error);
   });
 
-const GlobalEmailConfiguration = require('./schemas/GlobalEmailConfigurationSchema.js');
-const Language = require('./schemas/LanguageSchema.js');
-const Session = require('./schemas/SessionSchema.js');
-const NotificationEmails = require('./schemas/NotificationEmailsSchema.js');
-const NotificationEmailQueue = require('./schemas/NotificationEmailQueueSchema.js');
-const Tenancies = require('./schemas/tenanciesSchema.js');
-const TenanciesTypes = require('./schemas/TenanciesTypesSchema.js');
+const GlobalEmailConfiguration = require('./schemas/Other/GlobalEmailConfigurationSchema.js');
+const Language = require('./schemas/Other/LanguageSchema.js');
+const Session = require('./schemas/Other/SessionSchema.js');
+const NotificationEmails = require('./schemas/Other/NotificationEmailsSchema.js');
+const NotificationEmailQueue = require('./schemas/Other/NotificationEmailQueueSchema.js');
+const Tenancies = require('./schemas/Other/tenanciesSchema.js');
+const TenanciesTypes = require('./schemas/Other/TenanciesTypesSchema.js');
 const User = require('./schemas/Users/UserSchema.js');
 const UserGroup = require('./schemas/Users/UserGroupSchema.js');
-const RegistrationKey = require('./schemas/RegistrationKeySchema.js');
-const Images = require('./schemas/ImagesSchema.js');
-const ImagesCategories = require('./schemas/ImagesCategoriesSchema.js');
+const RegistrationKey = require('./schemas/Other/RegistrationKeySchema.js');
+const Images = require('./schemas/Other/ImagesSchema.js');
+const ImagesCategories = require('./schemas/Other/ImagesCategoriesSchema.js');
 const Casino = require('./schemas/Casinos/CasinoSchema.js');
 const CasinoTags = require('./schemas/Casinos/CasinoTagsSchema.js');
 const CasinoFeatures = require('./schemas/Casinos/CasinoFeaturesSchema.js');
@@ -31,11 +31,11 @@ const CasinoPaymentMethods = require('./schemas/Casinos/CasinoPaymentMethodsSche
 const CasinoWagerTypes = require('./schemas/Casinos/CasinoWagerTypesSchema.js');
 const CasinoCategories = require('./schemas/Casinos/CasinoCategoriesSchema.js');
 const CasinoLicenses = require('./schemas/Casinos/CasinoLicensesSchema.js');
-const ShortLinks = require('./schemas/ShortLinksSchema.js');
-const ShortLinksHits = require('./schemas/ShortLinksHitsSchema.js');
-const ShortLinksStatistics = require('./schemas/ShortLinksStatisticsSchema.js');
+const ShortLinks = require('./schemas/Other/ShortLinksSchema.js');
+const ShortLinksHits = require('./schemas/Other/ShortLinksHitsSchema.js');
+const ShortLinksStatistics = require('./schemas/Other/ShortLinksStatisticsSchema.js');
 
-const CasinoWishListBot = require('./schemas/CasinoWishListBotSchema.js');
+const CasinoWishListBot = require('./schemas/Other/CasinoWishListBotSchema.js');
 
 const ProxmoxServers = require('./schemas/Proxmox/ProxmoxServersSchema.js');
 const ProxmoxServersLogs = require('./schemas/Proxmox/ProxmoxServersSchema.js');
@@ -50,7 +50,7 @@ const ServicesActive = require('./schemas/Services/ServicesActiveSchema.js');
 const UserPoints = require('./schemas/Users/UserPointsSchema.js');
 const UserPointsHistory = require('./schemas/Users/UserPointsHistorySchema.js');
 
-const HcVms = require('./schemas/HcVmSchema.js');
+const HcVms = require('./schemas/Other/HcVmSchema.js');
 
 module.exports = {
     GlobalEmailConfiguration,
