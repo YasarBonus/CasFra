39004cb31dd9ab8b8c67080dc0bc44f0d96ee15a
Joshua Treudler <joshua@treudler.net>
2024-01-06 00:50:56 +0100

    Add shipNothing function to handle order processing

---
+++ src/modules/Adapter/Nothing.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const db = require('../../db/database.js');
+
+async function shipNothing(orderId) {
+
+    const ServicesOrders = mongoose.model('ServicesOrders');
+    const order = await ServicesOrders.findOne({ _id: orderId });
+    if (order) {
+        order.status.status = 'confirmed';
+        order.status.date = Date.now();
+        await order.save();
+        console.log(`Order ${orderId} processed`);
+    } else {
+        console.log(`Order ${orderId} not found`);
+    }
+}
+
+process.on('message', (message) => {
+    const run = shipNothing(message.orderId);
+
+    run.then(() => {
+        process.exit(0, 'success');
+    }
+    ).catch((err) => {
+        console.log(err);
+        process.exit(1, 'error:' + err);
+    });
+} );
+
