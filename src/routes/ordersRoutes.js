6ff87b82e7231922fa893df7272ecbee67326fb6
Joshua Treudler <joshua@treudler.net>
2024-01-05 16:23:31 +0100

    Refactor available-services routes and add new order creation route

--- src/routes/ordersRoutes.js
+++ src/routes/ordersRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const logger = require('../modules/winston.js');
 const db = require('../db/database.js');
-const bodymen = require('bodymen'); 
+const bodymen = require('bodymen');
 
 const checkPermissions = require('../middlewares/permissionMiddleware.js');
 
@@ -15,7 +15,10 @@ const checkPermissions = require('../middlewares/permissionMiddleware.js');
 
 router.get('/available-services', async (req, res) => {
     try {
-        const services = await db.Services.find({ orderable: true, active: true }).populate('type');
+        const services = await db.Services.find({
+            orderable: true,
+            active: true
+        }).populate('type');
         // do not return orderable and active fields
         services.forEach((service) => {
             service.orderable = undefined;
@@ -25,9 +28,11 @@ router.get('/available-services', async (req, res) => {
         res.json(services);
     } catch (err) {
         logger.error(err);
-        res.status(500).json({ message: err.message });
+        res.status(500).json({
+            message: err.message
+        });
     }
-} );
+});
 
 // Get all services that can be ordered (orderable = true and active = true) of a specific type
 // Permissions: none
@@ -40,13 +45,16 @@ router.get('/available-services', async (req, res) => {
 router.get('/available-services/:type', async (req, res) => {
     try {
 
-        const services = await db.Services.find({ orderable: true, active: true }).populate('type');
-        
+        const services = await db.Services.find({
+            orderable: true,
+            active: true
+        }).populate('type');
+
         // filter the services by type name
         const filteredServices = services.filter((service) => {
             return service.type.name === req.params.type;
-        } );
-        
+        });
+
         // do not return orderable and active fields
         filteredServices.forEach((service) => {
             service.orderable = undefined;
@@ -56,24 +64,99 @@ router.get('/available-services/:type', async (req, res) => {
         res.json(filteredServices);
     } catch (err) {
         logger.error(err);
-        res.status(500).json({ message: err.message });
+        res.status(500).json({
+            message: err.message
+        });
     }
-} );
+});
 
-//
-// Create a new order for a service for the current user
-// if the service is orderable = true and active = true.
-// If a tenant is specified, the tenant needs to be active = true,
-// the tenant needs to be in the user's tenants list
 // Permissions: orderServices
-// POST /service/:id
-// This will create a new order for the service with the id :id
-// The order will be created for the current user (user_id),
-// if a tenant is specified, the order will be created for the tenant (tenant_id)
+// POST /service
+// Create a new order for a service for the current user
+// The user has to specify the service id or shortname, the interval, and the tenant (optional)
+// If a tenant is specified, the tenant needs to be active = true, and in the user.tenancies
+// The service needs to be orderable = true and active = true.
 // The order will be created with:
-// order.status = 'pending', order.completed = false
-// creation_date = now, creation_ip = req.ip
-// service: :id, user: req.user.id
+// order.status.status = 'new', order.status.date = now
+// creation_date = now, creation_ip = req.ip, creation_user = userId
+// service: serviceId, internal: service.internal
+// if a tenant is specified, the tenant will be set as tenant for the order,
+// else if no tenant is specified, the user will be set as user for the order
+// The order will be created with a random order number
+// The order number will be unique
+// The order number will be 10 characters long
+
+router.post('/service', bodymen.middleware({
+    service: db.Services.schema
+}), async (req, res) => {
+    const userId = req.session.user.userId;
+    const {
+        service,
+        interval,
+        tenant
+    } = req.body;
+
+    try {
+        const serviceData = await db.Services.findOne({
+            $or: [{
+                    _id: service
+                },
+                {
+                    shortname: service
+                }
+            ]
+        });
+        if (!serviceData) {
+            res.status(404).json({
+                message: 'Service not found'
+            });
+            return;
+        }
+        if (!serviceData.orderable || !serviceData.active) {
+            res.status(400).json({
+                message: 'Service not orderable'
+            });
+            return;
+        }
+
+        let tenantData;
+        if (tenant) {
+            tenantData = await db.Tenancies.findOne({
+                _id: tenant,
+                active: true,
+            });
+            if (!tenantData) {
+                res.status(404).json({
+                    message: 'Tenant not found'
+                });
+                return;
+            }
+        }
+
+        const orderNumber = Math.random().toString(36).substr(2, 10);
+        const order = new db.ServicesOrders({
+            order_number: orderNumber,
+            user: tenantData ? undefined : userId,
+            tenant: tenantData ? tenantData._id : undefined,
+            service: serviceData._id,
+            creation_date: Date.now(),
+            creation_ip: req.ip,
+            creation_user: userId,
+            status: {
+                status: 'new',
+                date: Date.now(),
+            },
+            completed: false,
+        });
+        await order.save();
+        res.json(order);
+    } catch (err) {
+        logger.error(err);
+        res.status(500).json({
+            message: err.message
+        });
+    }
+});
 
 
 
