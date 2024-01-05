d8ac70117ded0578c1dfaa12b6ec0dade976fce7
Joshua Treudler <joshua@treudler.net>
2024-01-05 20:40:17 +0100

    Remove unique constraint on user field in UserPointsSchema.js

--- src/db/schemas/Users/UserPointsSchema.js
+++ src/db/schemas/Users/UserPointsSchema.js
@@ -4,7 +4,6 @@ const UserPointsSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        unique: true,
     },
     points: {
         type: Number,
@@ -14,53 +13,11 @@ const UserPointsSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+    last_tx: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
 });
 
 const UserPoints = mongoose.model('UserPoints', UserPointsSchema);
 
-const UserPointsHistorySchema = new mongoose.Schema({
-    tx_id: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    points: {
-        type: Number,
-        default: 0,
-    },
-    before: {
-        type: Number,
-        default: 0,
-    },
-    diff: {
-        type: Number,
-        default: 0,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
-    reason: {
-        type: String,
-        default: 'none',
-    },
-    by: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    by_ip: {
-        type: String,
-    },
-});
-
-const UserPointsHistory = mongoose.model('UserPointsHistory', UserPointsHistorySchema);
-
-module.exports = {
-    UserPoints,
-    UserPointsHistory,
-};
-
+module.exports = UserPoints;
