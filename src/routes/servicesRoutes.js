c3375278455adc51c6788ffc752b15373b1e1b04
Joshua Treudler <joshua@treudler.net>
2024-01-05 15:29:41 +0100

    Add API endpoints for retrieving user services

--- src/routes/servicesRoutes.js
+++ src/routes/servicesRoutes.js
@@ -7,11 +7,20 @@ const checkPermissions = require('../middlewares/permissionMiddleware.js');
 
 // We need the following API router endpoints:
 
-// Get all services
+// Get all services (super)
 // Permissions: manageServices
 // GET /
 // This will return all services
 
+/**
+ * @openapi
+ * /services/super:
+ *   get:
+ *     summary: Get all services
+ *     tags: [Services, Super]
+ *     security:
+ *       - super: []
+ */
 router.get('/super', checkPermissions('manageServices'), async (req, res) => {
     try {
         const services = await db.Services.find().populate('type');
@@ -21,5 +30,29 @@ router.get('/super', checkPermissions('manageServices'), async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 } );
+
+/**
+ * @openapi
+ * /services:
+ *   get:
+ *     summary: Get all services of the current user
+ *     tags: [Services]
+ */
+router.get('/', async (req, res) => {
+    const userId = req.session.user.userId;
+
+    try {
+        const services = await db.ServicesActive.find({ user_id: userId }).populate('service').populate('service.type');
+        
+        if (services.length === 0) {
+            res.status(404).json({ message: 'No services found for the user' });
+        } else {
+            res.json(services);
+        }
+    } catch (err) {
+        logger.error(err);
+        res.status(500).json({ message: err.message });
+    }
+});
 
 module.exports = router;
