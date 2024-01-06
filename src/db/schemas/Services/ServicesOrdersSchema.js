e71914b520f7db79ef7a64d08d71d23a076c2a8b
Joshua Treudler <joshua@treudler.net>
2024-01-06 19:09:01 +0100

    Add 'deliveryQueued' status to ServicesOrdersSchema

--- src/db/schemas/Services/ServicesOrdersSchema.js
+++ src/db/schemas/Services/ServicesOrdersSchema.js
@@ -6,7 +6,7 @@ const ServicesOrdersStatusSchema = new mongoose.Schema({
         required: true,
         enum: ['new', 'awaitingConfirmation', 'confirming', 'confirmed', 'awaitingAutomatedVerification', 'automatedVerificationInProgres',
         'awaitingManualVerification', 'manualVerificationInProgress',
-        'verified', 'awaitingPayment', 'processingPayment', 'paid', 'awaitingDelivery',
+        'verified', 'awaitingPayment', 'processingPayment', 'paid', 'awaitingDelivery', 'deliveryQueued',
         'processing', 'completed', 'cancelled'
         ],
         default: 'new',
