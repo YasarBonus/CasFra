1ef1bbfb0a1dd00a6f051ba64c0994f5373b1382
Joshua Treudler <joshua@treudler.net>
2024-01-06 20:24:39 +0100

    Update order status and add logging

--- src/modules/Tasks/Orders/updateOrderStatus.js
+++ src/modules/Tasks/Orders/updateOrderStatus.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 const db = require('../../../db/database.js');
 
+const logger = require('../../winston.js');
+
 // function to update the order status
 async function updateOrderStatus(orderId, status) {
     const ServicesOrders = mongoose.model('ServicesOrders');
     const order = await ServicesOrders.findOne({ _id: orderId });
     if (order) {
         order.status.status = status;
+        order.logs.push({ message: 'Order status changed to ' + status, level: 'info', date: Date.now() });
         order.status.date = Date.now();
         await order.save();
-        console.log(`Order ${orderId} updated to ${status}`);
+        logger.info(`updateOrderStatus: Order ${order._id} updated to ${status}`);
     } else {
-        console.log(`Order ${orderId} not found`);
+        console.log(`updateOrderStatus: Order ${orderId} not found`);
     }
 }
 
