8958b1f457e4ddb4e99df7d9865d34f6223b8787
Joshua Treudler <joshua@treudler.net>
2024-04-28 00:07:59 +0200

    Add TwitchCommandBotSchema.js for Twitch command bot functionality

---
+++ src/db/schemas/TwitchWishListBot/TwitchCommandBotSchema.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const TwitchWishListBot = require('./TwitchWishListBotSchema');
+const { TwitchWishListBotSettings } = require('../../database');
+
+const TwitchCommandBotMessagesSchema = new mongoose.Schema({
+    globalIdentifier: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+});
+
+const TwitchCommandBotMessages = mongoose.model('TwitchCommandBotMessages', TwitchCommandBotMessagesSchema);
+
+const TwitchCommandBotSettingsSchema = new mongoose.Schema({
+    TwitchWishListBotEnabled:
+    {
+        type: Boolean,
+        required: true,
+    },
+    twitchUsername: {
+        type: String,
+    },
+    twitchOauth: {
+        type: String,
+        required: true,
+    },
+    twitchChannel: {
+        type: String,
+        required: true,
+    },
+    messages: {
+        type: [TwitchCommandBotMessagesSchema],
+        required: true,
+    },
+});
